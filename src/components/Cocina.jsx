3f27a862f85346b0b50201dbd28046fab541d939
tamaramunoz <tami.skt@gmail.com>
2020-03-10 10:59:50 -0300

    agregando componentes mesonero y cocina

---
+++ src/components/Cocina.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+
+const Cocina = () => {
+    return ( 
+        <Fragment>
+             <h1>Cocina</h1>
+        </Fragment>
+       
+     );
+}
+ 
+export default Cocina;
